1d334e28124e6a37988d2c85a02fb80dbaf013e0
Marine SANJUAN <marine@sanjuan.eu>
2021-08-05 13:34:58 +0200

    negatives in print dec to hex are working, just had to cast the int in unsigned int

--- srcs/ft_printf_utils.c
+++ srcs/ft_printf_utils.c
@@ -6,7 +6,7 @@
 /*   By: msanjuan <msanjuan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/04 05:58:15 by msanjuan          #+#    #+#             */
-/*   Updated: 2021/08/05 13:26:22 by msanjuan         ###   ########.fr       */
+/*   Updated: 2021/08/05 13:34:05 by msanjuan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,64 +22,6 @@ void ft_putnbr_uint(unsigned int n, int fd)
 		ft_putnbr_uint((n % 10), fd);
 	}
 }
-
-// void ft_putnbr_hexa(int n, int fd)
-// {
-// 	char *base;
-// 	long int nbr;
-// 	int len;
-
-// 	base = "0123456789abcdef";
-// 	len = ft_strlen(base);
-// 	nbr = (long int)n;
-// 	if (nbr < 0)
-// 	{
-// 		nbr = (unsigned int)n;
-// 		ft_putnbr_hexa(base[nbr / 16], fd);
-// 	}
-// 	else
-// 	{
-// 		if (nbr >= len)
-// 		{
-// 			ft_putnbr_hexa(base[nbr / 16], fd);
-// 			ft_putchar_fd(base[nbr % 16], fd);
-// 			nbr /= 16;
-// 		}
-// 		else
-// 		{
-// 			ft_putchar_fd(base[nbr % 16], fd);
-// 			nbr /= 16;
-// 		}
-// 	}
-// // }
-// void ft_putnbr_hexa(int n, int fd)
-// {
-// 	char *base;
-// 	long int nbr;
-	
-// 	base = "0123456789abcdef";
-// 	nbr = (long int)n;
-// 	if (nbr == 0) 
-// 	{
-// 		ft_putchar_fd('0', fd);
-// 	}
-// 	if (nbr > 0) {
-// 		while (nbr) 
-// 		{
-// 			ft_putchar_fd(base[nbr % 16], fd);
-// 			nbr /= 16;
-// 		}
-// 	}
-// 	else 
-// 	{
-// 		unsigned int num = n;
-// 		while (num) 
-// 		{
-// 			ft_putchar_fd(base[num % 16], fd);
-// 			num /= 16;
-// 		}
-// 	}
-// }
 
 void ft_putnbr_hexa(int n, int fd)
 {
