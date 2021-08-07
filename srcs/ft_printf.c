7a6182e28decd5529c101d1084760c9f04842d7a
Marine SANJUAN <marine@sanjuan.eu>
2021-08-07 11:39:59 +0200

    42 norm OK

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,22 +6,20 @@
 /*   By: msanjuan <msanjuan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/02 14:46:14 by msanjuan          #+#    #+#             */
-/*   Updated: 2021/08/06 14:35:10 by msanjuan         ###   ########.fr       */
+/*   Updated: 2021/08/07 11:38:41 by msanjuan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_printf(const char *flag_lst, ...)
+int	ft_printf(const char *flag_lst, ...)
 {
-	// Declaration avec va_start, va_arg pour recuperer les arguments
-	va_list args_lst;
-	size_t i;
-	int count;
-	int specifiers_count;
+	va_list	args_lst;
+	size_t	i;
+	int		count;
+	int		specifiers_count;
 
 	va_start(args_lst, flag_lst);
-	// Parse the arguments
 	i = 0;
 	count = 0;
 	specifiers_count = 0;
@@ -29,10 +27,10 @@ int ft_printf(const char *flag_lst, ...)
 	{
 		if (flag_lst[i] == '%')
 		{
-			specifiers_count += parse_format_specifiers(flag_lst[i + 1], args_lst);
+			specifiers_count += parse_fmt_specifiers(flag_lst[i + 1], args_lst);
 			i++;
-		} 
-		else 
+		}
+		else
 		{
 			write(1, &(flag_lst[i]), 1);
 			count++;
