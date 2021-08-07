aae0c7574ec8d8961b7ccb8d078d697302959b0c
Marine SANJUAN <marine@sanjuan.eu>
2021-08-07 11:55:53 +0200

    42 norm OK

--- srcs/print_characters.c
+++ srcs/print_characters.c
@@ -6,7 +6,7 @@
 /*   By: msanjuan <msanjuan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/03 18:15:01 by msanjuan          #+#    #+#             */
-/*   Updated: 2021/08/05 17:48:52 by msanjuan         ###   ########.fr       */
+/*   Updated: 2021/08/07 11:55:32 by msanjuan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ int	print_percentage(void)
 
 int	print_character(va_list arg)
 {
-	char temp;
+	char	temp;
+
 	temp = va_arg(arg, int);
 	ft_putchar_fd(temp, 1);
 	return (1);
@@ -28,18 +29,20 @@ int	print_character(va_list arg)
 
 int	print_string(va_list arg)
 {
-	char *temp;
-	int count;
+	char	*temp;
+	int		count;
 
 	count = 0;
 	temp = va_arg(arg, char *);
 	if (temp == NULL)
+	{
 		ft_putstr_fd("(null)", 1);
-	else
-		ft_putstr_fd(temp, 1);
-	if (temp == NULL)
 		return (6);
-	else 
+	}	
+	else
+	{
+		ft_putstr_fd(temp, 1);
 		count = ft_strlen(temp);
+	}
 	return (count);
 }
